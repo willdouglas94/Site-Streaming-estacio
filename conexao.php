7c37ef8ef9787ef3481f175367eb6a0cae63bc53
willdouglas94 <144810926+willdouglas94@users.noreply.github.com>
2024-06-04 18:39:49 -0300

    pre.

--- conexao.php
+++ conexao.php
@@ -7,6 +7,8 @@ $dbname = "cadastro";
 $conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
 if (!$conexao) {
     die("Houve um erro". mysqli_connect_error());
+} else {
+    echo"Conectado com sucesso";
 }
 
 ?>
